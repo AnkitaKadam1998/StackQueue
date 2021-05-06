ccb33044d0f8a057f4e1d9de89239c3bae938f04
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-05-06 19:26:04 +0530

    ADD:welcome to stack Queue problem

--- CreateStack.java
+++ CreateStack.java
@@ -5,12 +5,7 @@ import java.util.LinkedList;
 public class CreateStack 
 {
 		public static void main(String args[])
-		{
-			LinkedList<Integer> linkedList = new LinkedList<>();
-			linkedList.push(70);
-			linkedList.push(30);
-			linkedList.push(56);
-			
-			System.out.println(linkedList);
+		{	
+			System.out.println("welcome to stack Queue program");
 		}
 }
