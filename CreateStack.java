2b782afcb529194c34206d1a6d10956788047079
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-05-06 19:21:21 +0530

    ADD:create stack

---
+++ CreateStack.java
@@ -0,0 +1,16 @@
+package com.stackQueue;
+
+import java.util.LinkedList;
+
+public class CreateStack 
+{
+		public static void main(String args[])
+		{
+			LinkedList<Integer> linkedList = new LinkedList<>();
+			linkedList.push(70);
+			linkedList.push(30);
+			linkedList.push(56);
+			
+			System.out.println(linkedList);
+		}
+}
