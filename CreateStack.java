d72d107b03dddb6f1687ed0a527c2cb9cb2aaaac
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-05-06 19:31:55 +0530

    create stack using linkedlist

--- CreateStack.java
+++ CreateStack.java
@@ -5,7 +5,13 @@ import java.util.LinkedList;
 public class CreateStack 
 {
 		public static void main(String args[])
-		{	
-			System.out.println("welcome to stack Queue program");
+		{
+			LinkedList<Integer> linkedList = new LinkedList<>();
+			linkedList.push(70);
+			linkedList.push(30);
+			linkedList.push(56);
+			
+			System.out.println(linkedList);
 		}
-}
+	}
+
